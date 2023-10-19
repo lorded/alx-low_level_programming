1ca89c1c7d2d952ed500e4081c0deea8fd8aa243
lorded <mynadishon@gmail.com>
2023-10-20 01:21:21 +0300

    linked

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list.
+ * @head: list_t that is going to be let free
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+
+	{
+	temp = head->next;
+	freelist(head->str);
+	freelist(head);
+	head = temp;
+	}
+}
+
