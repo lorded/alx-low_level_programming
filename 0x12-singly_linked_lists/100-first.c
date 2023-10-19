775fe69db2a131fd31145d6e0af624606a08943d
lorded <mynadishon@gmail.com>
2023-10-20 02:35:29 +0300

    linked

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_message - prints a message
+ */
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
+/**
+ * main - the entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	print_message();
+
+	return (0);
+}
+
