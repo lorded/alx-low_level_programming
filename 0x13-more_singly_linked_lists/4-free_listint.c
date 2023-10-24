1ded2304d941d332ecf48c31433c0dd6bc13885f
lorded <mynadishon@gmail.com>
2023-10-24 23:08:35 +0300

    linkedlists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees the memory of linked list
+ * @head: A pointer to the listint_t to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current_node;
+
+	while (head)
+	{
+		current_node = head->next;
+		current_node(head);
+		head = current_node;
+	}
+}
+
