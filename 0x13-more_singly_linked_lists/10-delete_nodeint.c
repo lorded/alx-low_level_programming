45d1f356c712d27525ddc46fd0d22fa898d44e3f
lorded <mynadishon@gmail.com>
2023-10-25 00:24:36 +0300

    linkedlists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -33,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 
 	nodeto_bedeleted = temp->next;
-	temp->next = nodeto_bedeleted->next;
+	nodeto_bedeleted->next = nodeto_bedeleted->next;
 	free(nodeto_bedeleted);
 
 	return (1);
