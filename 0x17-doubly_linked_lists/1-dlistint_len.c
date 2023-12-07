f3ce468977a073c261dca2dccc709461022c47f5
lorded <mynadishon@gmail.com>
2023-12-08 02:46:13 +0300

    linked

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,19 +9,19 @@
 size_t dlistint_len(const dlistint_t *h)
 {
     /*Variable to store the number of elements in the list.*/
-    size_t nodes = 0;
+	size_t nodes = 0;
 
-    while (h)
-    {
-        /*Increment the element count.*/
-        nodes++;
+	while (h)
+	{
+	/*Increment the element count.*/
+	nodes++;
 
 
-        /*Move to the next element.*/
-        h = h->next;
-    }
+	/*Move to the next element.*/
+	h = h->next;
+	}
 
     /*Return the total number of elements in the list.*/
-    return (nodes);
+	return (nodes);
 }
 
