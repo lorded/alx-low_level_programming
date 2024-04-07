5fc7e8e90695606864fc73d8c8f71401d8974a48
lorded <mynadishon@gmail.com>
2024-04-07 17:48:59 +0300

    dynam lib

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 gcc *.c -c -fPIC
-gcc *.o -shared -o liball.so
+gcc *.o -shared -o liball.s#!/bin/bash
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
